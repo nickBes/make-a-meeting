3af1e1189f21e6468fa4bd259e86276d01bac981
nickBes <58567842+nickBes@users.noreply.github.com>
2022-08-23 22:32:31 +0300

    now have a planned tech stack

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,8 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import React from "react"
+import type { AppProps } from "next/app"
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const app : React.FC<AppProps> = ({Component, pageProps}) => {
+    return <Component {...pageProps}/>
 }
 
-export default MyApp
+export default app
