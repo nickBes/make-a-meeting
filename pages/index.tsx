71a2c417a11451cba13d4bb8561f5d53f4ac737a
nickBes <58567842+nickBes@users.noreply.github.com>
2022-08-29 16:46:13 +0300

    added schemas for the meetings

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import { useSession, signIn, signOut } from "next-auth/react"
 import React from "react"
 
 const Home: React.FC = () => {
-    const { data: session, status } = useSession()
+    const { data: session } = useSession()
 
     if (session) {
         return <button onClick={() => signOut()}>Sign Out</button>
